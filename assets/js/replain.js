100569f6444a89e5ff1fed382565a201e65f3a1c
iRavshan <ravshan4ikov@gmail.com>
2022-08-11 15:34:20 +0500

    first commit

---
+++ assets/js/replain.js
@@ -0,0 +1,4 @@
+window.replainSettings = { id: 'b4802aba-e84e-4300-8ed4-5364198f3e5d' };
+(function(u){var s=document.createElement('script');s.async=true;s.src=u;
+var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+})('https://widget.replain.cc/dist/client.js');
