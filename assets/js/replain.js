f48ae5db1e9d5e0d52ab750d16d655e640dd8a61
Sadikov Dev <95114408+iRavshan@users.noreply.github.com>
2022-10-26 11:25:46 +0500

    Update replain.js

--- assets/js/replain.js
+++ assets/js/replain.js
@@ -1,4 +1,4 @@
-window.replainSettings = { id: 'b4802aba-e84e-4300-8ed4-5364198f3e5d' };
+window.replainSettings = { id: '94a24384-84f2-4369-8c3c-a7ae5b21e0a7' };
 (function(u){var s=document.createElement('script');s.async=true;s.src=u;
 var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
 })('https://widget.replain.cc/dist/client.js');
